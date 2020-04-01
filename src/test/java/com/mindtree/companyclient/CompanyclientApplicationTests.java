81b6e338b96b5261cfc55f6e283a1cc50b2f374e
karthiktr147 <karthiktr147@gmail.com>
2020-04-02 03:50:03 +0530

    companyclient

---
+++ src/test/java/com/mindtree/companyclient/CompanyclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.companyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
